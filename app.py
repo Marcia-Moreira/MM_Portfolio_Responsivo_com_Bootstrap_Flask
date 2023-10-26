ee04676b4a1a799833873e063876229597b5a37c
Marcia-Moreira <mm.no.linkedin@gmail.com>
2023-10-26 15:52:05 -0300

    Sobre-Texto-Tabela-Scroolbar-Ancoragem

--- app.py
+++ app.py
@@ -8,11 +8,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# # erro:
-# @app.route('/')
-# def sobre():
-#     return render_template('sobre.html')
-
 # especifica e roda apenas se for app na main
 if __name__ == '__main__':
     app.run(debug=True)
