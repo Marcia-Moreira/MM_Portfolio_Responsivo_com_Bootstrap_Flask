b1b0c63d47d37692590dae83e19e6a6129b94a06
Marcia-Moreira <mm.no.linkedin@gmail.com>
2023-10-25 23:19:02 -0300

    configuracao, menu, inicio e hamburguer prontos

---
+++ app.py
@@ -0,0 +1,18 @@
+# Importar a Biblioteca Flask e a render_template (renderiza uma pag navegador...)
+
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# # erro:
+# @app.route('/')
+# def sobre():
+#     return render_template('sobre.html')
+
+# especifica e roda apenas se for app na main
+if __name__ == '__main__':
+    app.run(debug=True)
