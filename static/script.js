d9fd77bf2f5cbb4658554adc2f66e521e10e42f0
Marcia-Moreira <mm.no.linkedin@gmail.com>
2023-10-27 16:10:13 -0300

    links na tela principal e skills prontas

--- static/script.js
+++ static/script.js
@@ -1,3 +1,7 @@
+
+// =============================================================================================================
+// BootsTrap - Sistema de Grid Padrão
+
 // Abre e fecha menu lateral em modo mobile
 
 const menuMobile = document.querySelector('.menu-mobile')
@@ -10,4 +14,35 @@ menuMobile.addEventListener('click', () => {
     body.classList.toggle("menu-nav-active");
 });
 
-// =====================================================================
+// =============================================================================================================
+
+// Fechar o menu quando clicar em algum item/link da barra lateral e muda o ícone Hamburguer para List (pois antes está em X)
+
+// Vamos usar a class nav-item do asaid:
+
+// Vamos pegar o nav.item:
+const navItem = document.querySelectorAll('.nav-item')
+// Podemos verificar pedindo um console.log no inspencionar
+// console.log(navItem)
+// ele traz um array de todos nav-item
+navItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    })
+})
+
+// =============================================================================================================
+
+
+
+
+
+
+// =============================================================================================================
+
+// =============================================================================================================
+
+// =============================================================================================================
